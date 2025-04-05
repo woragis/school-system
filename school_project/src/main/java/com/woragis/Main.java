3a05a29988f02e3f372ea82f8b3918b2ddf2f293
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-05 00:27:39 -0300

    Use LinkedList to store university disciplines

--- school_project/src/main/java/com/woragis/Main.java
+++ school_project/src/main/java/com/woragis/Main.java
@@ -3,15 +3,21 @@ package com.woragis;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        LinkedList myList = new LinkedList(0);
-        myList.add(20);
-        myList.add(30);
-        myList.add(50);
-        myList.add(60);
-        myList.add(70);
-        myList.add(80);
-        myList.iterateList();
-        myList.remove(30);
-        myList.iterateList();
+        // LinkedList myList = new LinkedList(0);
+        // myList.add(20);
+        // myList.add(30);
+        // myList.add(50);
+        // myList.add(60);
+        // myList.add(70);
+        // myList.add(80);
+        // myList.iterateList();
+        // myList.remove(30);
+        // myList.iterateList();
+
+        LinkedList disciplinas = new LinkedList(null);
+        disciplinas.add(new Course("Data Structures", "Wallace"));
+        disciplinas.add(new Course("Object Oriented Programming", "Alvaro"));
+        disciplinas.add(new Course("SQL with MySql", "Alvaro"));
+        disciplinas.iterateList();
     }
 }
