58423dcd2492f041b5a254b3bb0d397b5a1b1f13
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-05 18:20:55 -0300

    Test new Students class and its methods

--- school_project/src/main/java/com/woragis/Main.java
+++ school_project/src/main/java/com/woragis/Main.java
@@ -18,16 +18,34 @@ public class Main {
         // disciplinas.add(new Course("Object Oriented Programming", "Alvaro"));
         // disciplinas.add(new Course("SQL with MySql", "Alvaro"));
         // disciplinas.iterateList();
-        // Student jezreel = new Student("Jezreel", "378");
+        Student jezreel = new Student("Jezreel", "378");
 
-        // jezreel.addCourse(new Course("Data Structures", "Wallace"));
-        // jezreel.addCourse(new Course("Object Oriented Programming with Java",
-        // "Joe"));
-        // jezreel.addCourse(new Course("Sql with MySql", "Alvaro"));
-        // jezreel.addCourse(new Course("Objective C", "Leonardo"));
+        jezreel.addCourse(new Course("Data Structures", "Wallace"));
+        jezreel.addCourse(new Course("Object Oriented Programming with Java",
+                "Joe"));
+        jezreel.addCourse(new Course("Sql with MySql", "Alvaro"));
+        jezreel.addCourse(new Course("Objective C", "Leonardo"));
 
         // jezreel.getCourses();
+        Student carlos = new Student("Carlos", "377");
 
-        Menu.mainloop();
+        carlos.addCourse(new Course("Data Structures", "Wallace"));
+        carlos.addCourse(new Course("Object Oriented Programming with Java",
+                "Joe"));
+        carlos.addCourse(new Course("Sql with MySql", "Alvaro"));
+        carlos.addCourse(new Course("Objective C", "Leonardo"));
+
+        // carlos.getCourses();
+
+        // Menu.mainloop();
+        Students unipe = new Students(1);
+        try {
+            unipe.add(jezreel);
+            unipe.increaseSize(2);
+            unipe.add(carlos);
+            unipe.view();
+        } catch (Exception e) {
+            System.out.println("Could not add more students");
+        }
     }
 }
