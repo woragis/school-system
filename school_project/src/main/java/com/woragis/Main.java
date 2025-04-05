693b3700f39c3e8b6a46d6e772a7a370597f9d3f
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-05 00:33:25 -0300

    Implement linked list to student courses

--- school_project/src/main/java/com/woragis/Main.java
+++ school_project/src/main/java/com/woragis/Main.java
@@ -14,10 +14,18 @@ public class Main {
         // myList.remove(30);
         // myList.iterateList();
 
-        LinkedList disciplinas = new LinkedList(null);
-        disciplinas.add(new Course("Data Structures", "Wallace"));
-        disciplinas.add(new Course("Object Oriented Programming", "Alvaro"));
-        disciplinas.add(new Course("SQL with MySql", "Alvaro"));
-        disciplinas.iterateList();
+        // LinkedList disciplinas = new LinkedList(null);
+        // disciplinas.add(new Course("Data Structures", "Wallace"));
+        // disciplinas.add(new Course("Object Oriented Programming", "Alvaro"));
+        // disciplinas.add(new Course("SQL with MySql", "Alvaro"));
+        // disciplinas.iterateList();
+        Student jezreel = new Student("Jezreel", "378");
+
+        jezreel.addCourse(new Course("Data Structures", "Wallace"));
+        jezreel.addCourse(new Course("Object Oriented Programming with Java", "Joe"));
+        jezreel.addCourse(new Course("Sql with MySql", "Alvaro"));
+        jezreel.addCourse(new Course("Objective C", "Leonardo"));
+
+        jezreel.getCourses();
     }
 }
--- school_project/src/main/java/com/woragis/Student.java
+++ school_project/src/main/java/com/woragis/Student.java
@@ -2,10 +2,12 @@ package com.woragis;
 
 public class Student extends Person {
     private String rgm;
+    private LinkedList courses;
 
     public Student(String name, String rgm) {
         this.name = name;
         this.rgm = rgm;
+        this.courses = new LinkedList(null);
     }
 
     public String getName() {
@@ -14,6 +16,18 @@ public class Student extends Person {
 
     public String getRgm() {
         return rgm;
+    }
+
+    public void addCourse(Course course) {
+        this.courses.add(course);
+    }
+
+    public void removeCourse(Course course) {
+        this.courses.remove(course);
+    }
+
+    public void getCourses() {
+        this.courses.iterateList();
     }
 
 }
