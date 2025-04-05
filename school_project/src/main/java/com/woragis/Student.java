678ddb95ed8780a0605ca5b145d0ef639d274c58
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-05 00:14:01 -0300

    Create student type

---
+++ school_project/src/main/java/com/woragis/Student.java
@@ -0,0 +1,20 @@
+package com.woragis;
+
+public class Student {
+    private String name;
+    private String rgm;
+
+    public Student(String name, String rgm) {
+        this.name = name;
+        this.rgm = rgm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRgm() {
+        return rgm;
+    }
+
+}
