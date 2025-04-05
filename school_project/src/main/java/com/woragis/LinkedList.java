e4ecb6903e5cb76ee616688a6b06fe3802838044
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-04 23:55:19 -0300

    Implement removing element from linked list

--- school_project/src/main/java/com/woragis/LinkedList.java
+++ school_project/src/main/java/com/woragis/LinkedList.java
@@ -15,11 +15,28 @@ public class LinkedList {
             index++;
             current = current.next;
         }
-        return;
     }
 
     public void add(int value) {
         LinkedListNode newHead = new LinkedListNode(value, this.head);
         this.head = newHead;
+    }
+
+    public void pop() {
+        this.head = this.head.next;
+    }
+
+    public void remove(int value) {
+        LinkedListNode current = this.head;
+        LinkedListNode previous = current;
+
+        while (current != null) {
+            if (current.value == value) {
+                previous.next = current.next;
+            } else {
+                previous = current;
+            }
+            current = current.next;
+        }
     }
 }
--- school_project/src/main/java/com/woragis/Main.java
+++ school_project/src/main/java/com/woragis/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         LinkedList myList = new LinkedList(0);
-        myList.iterateList();
         myList.add(20);
+        myList.add(30);
+        myList.add(50);
+        myList.add(60);
+        myList.add(70);
+        myList.add(80);
+        myList.iterateList();
+        myList.remove(30);
         myList.iterateList();
     }
 }
