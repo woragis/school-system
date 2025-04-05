2aceff83d4b627b3ce7454aa1c79baa45b57a9f5
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-05 18:20:08 -0300

    Create vector abstract class

---
+++ school_project/src/main/java/com/woragis/Vector.java
@@ -0,0 +1,18 @@
+package com.woragis;
+
+public class Vector<T> {
+    protected T[] values;
+    protected int size;
+
+    protected void add(T newValue) throws Exception {
+    }
+
+    protected void remove(T newValue) throws Exception {
+    }
+
+    protected void view() throws Exception {
+    }
+
+    protected void order() throws Exception {
+    }
+}
