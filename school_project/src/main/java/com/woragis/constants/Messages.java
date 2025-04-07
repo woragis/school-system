fadd24c8eb3b80534e157a1e1d85781e7814e0b2
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-07 20:32:30 -0300

    Create messages constants

---
+++ school_project/src/main/java/com/woragis/constants/Messages.java
@@ -0,0 +1,34 @@
+package com.woragis.constants;
+
+public class Messages {
+
+    // Used in main class
+    public static final String SHOWING_STUDENTS = "Mostrando alunos...";
+    public static final String ADDING_STUDENTS = "Adicionando aluno...";
+    public static final String SEARCHING_STUDENTS = "Pesquisando aluno...";
+    public static final String DELETING_STUDENTS = "Deletando aluno...";
+
+    public static final String EXITING = "Saindo do programa...";
+
+    // Options in main class
+    // Main options
+    public static final String MAIN_OPTION_1 = "[1]-Mostrar alunos";
+    public static final String MAIN_OPTION_2 = "[2]-Adicionar aluno";
+    public static final String MAIN_OPTION_3 = "[3]-Pesquisar aluno";
+    public static final String MAIN_OPTION_4 = "[4]-Deletar aluno";
+    public static final String MAIN_OPTION_5 = "[5]-Sair\n";
+
+    // Student options
+    public static final String STUDENT_OPTION_1 = "[1]-Adicionar Materia";
+    public static final String STUDENT_OPTION_2 = "[2]-Remover Materia";
+    public static final String STUDENT_OPTION_3 = "[3]-Deletar aluno";
+    public static final String STUDENT_OPTION_4 = "[4]-Voltar\n";
+
+    public static final String GET_STUDENT_NAME = "Nome do aluno: ";
+    public static final String GET_STUDENT_RGM = "Rgm do aluno: ";
+
+    // Students Class
+    public static final String INCREASING_CAPACITY = "Aumentando capacidade de armazenamento.";
+    public static final String STUDENT = "Estudante";
+    public static final String FOUND_STUDENT = "Estudante encontrado.";
+}
