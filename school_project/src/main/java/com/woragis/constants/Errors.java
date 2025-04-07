da30b2f5dca37b4b699426b5b4877adb59939252
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-07 20:32:44 -0300

    Create errors constants

---
+++ school_project/src/main/java/com/woragis/constants/Errors.java
@@ -0,0 +1,24 @@
+package com.woragis.constants;
+
+public class Errors {
+    // Main class
+    public static final String SHOWING_STUDENTS_ERROR = "Erro ao tentar mostrar alunos.";
+    public static final String ADDING_STUDENT_ERROR = "Erro ao tentar adicionar aluno.";
+    public static final String SEARCHING_STUDENT_ERROR = "Erro ao tentar pesquisar aluno.";
+    public static final String DELETE_STUDENT_ERROR = "Erro ao tentar deletar aluno.";
+    public static final String INVALID_CHAR_ERROR = "Voce digitou um caractere invalido";
+
+    public static final String _STUDENTS_ERROR = "Erro ao tentar deletar aluno.";
+    public static final String _STUDENT_ERROR = "Erro ao tentar deletar aluno.";
+
+    // Students class
+    public static final String STUDENT_NOT_FOUND_ERROR = "Estudante nao encontrado";
+    public static final String EMPTY_STUDENTS_ERROR = "Lista vazia";
+    public static final String FULL_STUDENTS_ERROR = "Lista cheia";
+
+    public static final String SAVING_ERROR = "Erro salvando backup";
+    public static final String LOADING_ERROR = "Erro carregando backup";
+
+    public static final String STUDENT_NOT_FOUND = "Estudante nao encontrado";
+
+}
