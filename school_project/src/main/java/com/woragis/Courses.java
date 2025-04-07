069f6ad5dc3a9dd962ea0a31f24c14d89203503d
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-07 20:36:09 -0300

    Create specific linked list focused on storing courses

---
+++ school_project/src/main/java/com/woragis/Courses.java
@@ -0,0 +1,43 @@
+package com.woragis;
+
+public class Courses
+        extends LinkedList<Course> {
+    public Courses() {
+        this.head = new LinkedListNode<Course>(null);
+    }
+
+    @Override
+    public void add(Course value) {
+        super.add(value);
+    }
+
+    @Override
+    public void iterateList() {
+        super.iterateList();
+    }
+
+    @Override
+    public void pop() {
+        super.pop();
+    }
+
+    public void remove(String courseName) {
+        LinkedListNode<Course> current = this.head;
+        LinkedListNode<Course> previous = current;
+
+        while (current != null) {
+            if (current.getValue().getName() == courseName) {
+                previous.setNext(current.getNext());
+            } else {
+                previous = current;
+            }
+            current = current.getNext();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
